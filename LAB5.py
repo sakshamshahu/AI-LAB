4f674ac9502f19317faded40f9564640fe2b28c1
Saksham <sakshamshahu.work@gmail.com>
2022-09-20 20:27:58 +0530

    Updated

--- LAB5.py
+++ LAB5.py
@@ -9,25 +9,17 @@ IrisDataframe = pd.DataFrame(Raw)
 print(IrisDataframe)
 
 SepalWidth_Hist = sns.histplot(data =IrisDataframe, x='SepalWidthCm')
-plt.show()
 
 SepalLength_Hist = sns.histplot(data =IrisDataframe, x='SepalLengthCm')
-plt.show()
 
 PetalLengthCm_Hist = sns.histplot(data =IrisDataframe, x='PetalLengthCm')
-plt.show()
 
 PetalWidthCm_Hist = sns.histplot(data =IrisDataframe, x='PetalWidthCm')
-plt.show()
 
 ll=sns.jointplot(data=IrisDataframe, x='SepalLengthCm', y='PetalLengthCm', hue ='Species')
-plt.show()
 
 tt=sns.scatterplot(data=IrisDataframe, x='SepalLengthCm', y='PetalLengthCm', hue ='Species')
-plt.show()
 
 box_plot =sns.boxplot(data=IrisDataframe, y= 'PetalLengthCm',x='Species', hue='Species')
-plt.show()
 
 violin_plot =sns.violinplot(data=IrisDataframe, y= 'PetalLengthCm',x='Species', hue='Species')
-plt.show()

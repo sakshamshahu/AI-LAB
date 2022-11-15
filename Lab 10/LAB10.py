7c0f853311957c8f312fc39bdfe04e1c6136b134
Saksham-Shahu <sakshamshahu.work@gmail.com>
2022-11-15 07:45:15 +0530

    fix: accuracy

--- Lab 10/LAB10.py
+++ Lab 10/LAB10.py
@@ -1,13 +1,13 @@
 #Support Vector Machines 
 
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.model_selection import cross_val_score
 
 dataset = pd.read_csv("Iris.csv")
 dataset = dataset.replace({"Species":  {"Iris-setosa":1,"Iris-versicolor":2, "Iris-virginica":3}})
+dataset = dataset.drop(columns= "Id")
 print(dataset)
 X = dataset.iloc[:,:-1]
 y = dataset.iloc[:, -1].values
@@ -19,7 +19,6 @@ classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
 print(classifier.score(X_test,y_test)*100)
 print(classifier.score(X_train,y_train)*100)
-
 
 accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
 print("Accuracy: {:.2f} %".format(accuracies.mean()*100))

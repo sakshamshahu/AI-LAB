5688e506db7da6e099fae0b306d73d4c99cfde80
Saksham-Shahu <sakshamshahu.work@gmail.com>
2022-11-15 07:12:35 +0530

    feat: Lab 10 added

---
+++ Lab 10/LAB10.py
@@ -0,0 +1,25 @@
+#Support Vector Machines 
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score
+
+dataset = pd.read_csv("Iris.csv")
+dataset = dataset.replace({"Species":  {"Iris-setosa":1,"Iris-versicolor":2, "Iris-virginica":3}})
+print(dataset)
+X = dataset.iloc[:,:-1]
+y = dataset.iloc[:, -1].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+classifier = SVC(kernel = 'linear', random_state = 0)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+print(classifier.score(X_test,y_test)*100)
+print(classifier.score(X_train,y_train)*100)
+
+
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
